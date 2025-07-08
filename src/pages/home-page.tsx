cf39bfaa2853699a4de0bf5f77b016b3f69e6a3a
Azimkhon Makhbubov <drwho.spam@gmail.com>
2025-07-08 18:26:49 +0200

    add nav panel

---
+++ src/pages/home-page.tsx
@@ -0,0 +1,57 @@
+import { Button } from "@/common/components/button";
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+} from "@/common/components/navigation-menu";
+
+export const HomePage = () => {
+  return (
+    <div className="w-screen font-gilroy h-screen bg-[#F9EBFA] flex items-center flex-col">
+      <div className="w-full h-full max-h-[186px] py-[84px] px-40 flex items-center justify-between">
+        <NavigationMenu className="w-full h-full">
+          <NavigationMenuList>
+            <NavigationMenuItem className="focus:bg-transparent" value="home">
+              <NavigationMenuTrigger className="[&>svg]:hidden hover:bg-transparent px-6 bg-transparent font-gilroy-medium cursor-pointer focus:bg-transparent text-[#4F1650] text-sm leading-4 underline decoration-[#DD86DF] underline-offset-6 decoration-2">
+                Home
+              </NavigationMenuTrigger>
+            </NavigationMenuItem>
+            <NavigationMenuItem
+              className="focus:bg-transparent"
+              value="products"
+            >
+              <NavigationMenuTrigger className="[&>svg]:hidden px-6 bg-transparent font-gilroy-medium cursor-pointer focus:bg-transparent text-[#4F1650] text-sm leading-4">
+                Products
+              </NavigationMenuTrigger>
+            </NavigationMenuItem>
+            <NavigationMenuItem
+              className="focus:bg-transparent"
+              value="organization"
+            >
+              <NavigationMenuTrigger className="[&>svg]:hidden px-6 bg-transparent font-gilroy-medium cursor-pointer focus:bg-transparent text-[#4F1650] text-sm leading-4">
+                Organization
+              </NavigationMenuTrigger>
+            </NavigationMenuItem>
+            <NavigationMenuItem
+              className="focus:bg-transparent"
+              value="account"
+            >
+              <NavigationMenuTrigger className="[&>svg]:hidden px-6 bg-transparent font-gilroy-medium cursor-pointer focus:bg-transparent text-[#4F1650] text-sm leading-4">
+                Account
+              </NavigationMenuTrigger>
+            </NavigationMenuItem>
+            <NavigationMenuItem className="focus:bg-transparent" value="help">
+              <NavigationMenuTrigger className="[&>svg]:hidden px-6 bg-transparent font-gilroy-medium cursor-pointer focus:bg-transparent text-[#4F1650] text-sm leading-4">
+                Help
+              </NavigationMenuTrigger>
+            </NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenu>
+        <Button className="bg-[#DD86DF] text-[#1F1B20] hover:bg-[#DD86DF] hover:shadow-xl">
+          Call Jessica
+        </Button>
+      </div>
+    </div>
+  );
+};
