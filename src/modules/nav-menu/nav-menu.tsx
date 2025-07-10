628bbb5be9abe4440b9064ddba67cc4704409943
Azimkhon Makhbubov <drwho.spam@gmail.com>
2025-07-10 12:36:26 +0200

    wip

--- src/modules/nav-menu/nav-menu.tsx
+++ src/modules/nav-menu/nav-menu.tsx
@@ -18,15 +18,29 @@ export const NavMenu = () => {
           <NavigationMenuList>
             {buttons.map((button) => (
               <NavigationMenuItem
-                className="focus:bg-transparent"
+                className={cn(
+                  "focus:bg-transparent",
+                  button.value === "help" && "hover:animate-bounce"
+                )}
                 value={button.value}
                 key={button.value}
+                onClick={() => {
+                  if (button.value === "help") {
+                    window.open(
+                      "https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley",
+                      "_blank"
+                    );
+                  }
+                }}
               >
                 <NavigationMenuTrigger
                   className={cn(
-                    "[&>svg]:hidden hover:bg-transparent px-6 bg-transparent font-gilroy-medium cursor-pointer focus:bg-transparent text-[#4F1650] text-sm leading-4",
-                    window.location.pathname === button.path &&
-                      "underline decoration-[#DD86DF] underline-offset-6 decoration-2"
+                    "[&>svg]:hidden hover:bg-transparent px-6 bg-transparent font-gilroy-medium focus:bg-transparent text-[#4F1650] text-sm leading-4",
+                    window.location.pathname === button.path
+                      ? "underline decoration-[#DD86DF] underline-offset-6 decoration-2"
+                      : "",
+                    (button.value === "help" || button.value === "home") &&
+                      "cursor-pointer"
                   )}
                 >
                   {button.label}
